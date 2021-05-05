3d56f78eb21cf0a2629ebd2162b0cfc551587a04
Md-Nuruddin-Adnan <nuruddin.adnan1993@gmail.com>
2021-05-05 23:17:12 +0600

    dashboard

--- js/custom.js
+++ js/custom.js
@@ -49,11 +49,22 @@ $('#dateRange').daterangepicker({
 
 // collapse sidebar start
 {
-   let collapsedBtn = document.getElementById('btn-collapse')
+   let collapsedBtn = document.getElementById('btn-collapse');
+   let fullDiv =  collapsedBtn.parentElement;
+   let sideDiv = collapsedBtn.parentElement.parentElement.children[0]
+   let sideDivWidth = sideDiv.offsetWidth;
+
    collapsedBtn.addEventListener('click', function(e){
-      let fullDiv =  collapsedBtn.parentElement;
-      let sideDiv = collapsedBtn.parentElement.parentElement.children[0]
-      console.log(sideDiv);
+     sideDiv.classList.toggle('d-none');
+     fullDiv.classList.toggle('w-100');
+     if(collapsedBtn.children[0].style.transform !== 'rotate(180deg)'){
+         collapsedBtn.children[0].style.transform = 'rotate(180deg)'
+         fullDiv.style.maxWidth = '100%';
+     }else {
+         collapsedBtn.children[0].style.transform = 'rotate(0deg)'
+         fullDiv.style.maxWidth = `calc(100% - ${sideDivWidth}px)`;
+     }
+
    })
 }
 // collapse sidebar end
